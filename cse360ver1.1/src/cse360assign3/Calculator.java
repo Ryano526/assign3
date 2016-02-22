2add7d8cf11ae53e26d7a89daee90104f91ee04e
Ryan Ditsworth <ryanditsworth@cox.net>
2016-02-22 15:33:32 -0700

    version 3, implemented gethistory

--- cse360ver1.1/src/cse360assign3/Calculator.java
+++ cse360ver1.1/src/cse360assign3/Calculator.java
@@ -3,9 +3,11 @@ package cse360assign3;
 public class Calculator {
 
 	private int total;
+	String history;
 	
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
+
 	}
 	
 	public int getTotal () {
@@ -14,21 +16,25 @@ public class Calculator {
 	
 	public void add (int value) {
 		total+=value;	// adds value to total
+		history = history + " + " + value;
 	}
 	
 	public void subtract (int value) {
 		total-=value;	// subtracts value from total
+		history = history + " - " + value;
 	}
 	
 	public void multiply (int value) {
 		total = total*total*value;	// multiply total by total by value
+		history = history + " * " + value;
 	}
 	
 	public void divide (int value) {
 		total = total/value;		// divide total by the value
+		history = history + " / " + value;
 	}
 	
 	public String getHistory () {
-		return "";
+		return history;
 	}
 }
