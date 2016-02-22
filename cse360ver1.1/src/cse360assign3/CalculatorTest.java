256a14e1a5fbd22cad615af74b1a7265ba11af8f
Ryan Ditsworth <ryanditsworth@cox.net>
2016-02-22 15:02:14 -0700

    Calculator and junit tests for calculator

---
+++ cse360ver1.1/src/cse360assign3/CalculatorTest.java
@@ -0,0 +1,63 @@
+package cse360assign3;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+	
+	public void testCalculator()
+	{
+		Calculator calc = new Calculator();
+		assertNotNull(calc);
+	}
+	
+	public void testgetTotal()
+	{
+		fail("Not yet implemented");
+	}
+	
+	public void testAdd()
+	{
+		//Calculator calc = new Calculator();
+		//calc.add(3);
+		//assert(false);
+		fail("Not yet implemented");
+	}
+	
+	public void testSubtract()
+	{
+		fail("Not yet implemented");
+	}
+	
+	public void testMultiply()
+	{
+		fail("Not yet implemented");
+	}
+	
+	public void testDivide()
+	{
+		fail("Not yet implemented");
+	}
+	
+	public void testgetHistory()
+	{
+		fail("Not yet implemented");
+	}
+	//add subtract multiply divide gethistory
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
